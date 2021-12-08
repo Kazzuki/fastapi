98df84f557fadb410ec7ef972b49a7a4132a5602
Kazzuki <madakazuki@icloud.com>
2021-12-08 22:08:02 +0900

    finish session4 26

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+from typing import Optional
 from fastapi import FastAPI
+
 
 app = FastAPI()
 
-@app.get("/")
+# @はデコレータと言われる
+@app.get("/hello")
 async def index():
     return {"message" : "Hello World"}
+
+# パスパラメータ
+@app.get("/countries/{country_name}")
+async def country(country_name: str):
+    return {"message" : country_name}
+
+# クエリパラメータ
+@app.get("/anime")
+async def anime(anime_name: str = "kauzki", anime_no: int = 1):
+    return{
+        "anime_name" : anime_name,
+        "anime_no" : anime_no
+    }
+
+# 必須ではないオプションパラメータ
+@app.get("/sushi")
+async def sushi(sushi_name: Optional[str] = None, sushi_no: Optional[int] = None):
+    return{
+        "name" : sushi_name,
+        "no" : sushi_no
+    }
+ 
